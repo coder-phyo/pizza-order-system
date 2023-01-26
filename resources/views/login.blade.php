a62e67c571aac95ae173fa606db8e077d3d54333
vinnie <mymailaddress010020@gmail.com>
2023-01-27 00:57:21 +0630

    login,register process

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,24 +4,29 @@
 
 @section('content')
     <div class="login-form">
-        <form action="" method="post">
+        <form action="{{ route('login') }}" method="post">
+            @csrf
             <div class="form-group">
                 <label>Email Address</label>
                 <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
+                @error('email')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
             <div class="form-group">
                 <label>Password</label>
                 <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
-            </div>
+                @error('password')
+                <small class="text-danger">{{ $message }}</small @enderror </div>
 
-            <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign
-                in</button>
+                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign
+                    in</button>
 
         </form>
         <div class="register-link">
             <p>
                 Don't you have account?
-                <a href="{{ url('register') }}">Sign Up Here</a>
+                <a href="{{ route('auth#registerPage') }}">Sign Up Here</a>
             </p>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login');
+// login , register
+Route::redirect('/', 'loginPage');
+Route::get('loginPage', [AuthController::class, 'loginPage'])->name('auth#loginPage');
+Route::get('registerPage', [AuthController::class, 'registerPage'])->name('auth#registerPage');
+
+// admin
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    // categiry
+    Route::group(['prefix' => 'category'], function () {
+        Route::get('list', [CategoryController::class, 'list'])->name('category#name');
+    });
 });
 
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// user
