0b1df41a88720791999979791eb823b0b8cb19f4
vinnie <mymailaddress010020@gmail.com>
2023-02-13 02:28:41 +0630

    Ajax Sorting part 2 & part 1 error fix

--- resources/views/user/main/home.blade.php
+++ resources/views/user/main/home.blade.php
@@ -73,7 +73,7 @@
                                     <select value="sorting" id="sortingOption" class="btn btn-light dropdown-toggle"
                                         data-toggle="dropdown">
                                         <option value="">Choose Option...</option>
-                                        <option value="asc">Ascoending</option>
+                                        <option value="asc">Ascending</option>
                                         <option value="desc">Descending</option>
                                     </select>
                                     {{-- <div class="dropdown-menu dropdown-menu-right">
@@ -85,37 +85,40 @@
                         </div>
                     </div>
 
-                    @foreach ($pizza as $p)
-                        <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
-                            <div class="product-item bg-light mb-4" id="myForm">
-                                <div class="product-img position-relative overflow-hidden">
-                                    <img class="img-fluid w-100" style="height:220px"
-                                        src="{{ asset('storage/' . $p->image) }}" alt="">
-                                    <div class="product-action">
-                                        <a class="btn btn-outline-dark btn-square" href=""><i
-                                                class="fa fa-shopping-cart"></i></a>
-                                        <a class="btn btn-outline-dark btn-square" href=""><i
-                                                class="fa-solid fa-circle-info"></i></a>
+                    <div class="row" id="dataList">
+                        @foreach ($pizza as $p)
+                            <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
+                                <div class="product-item bg-light mb-4">
+                                    <div class="product-img position-relative overflow-hidden">
+                                        <img class="img-fluid w-100" style="height:220px"
+                                            src="{{ asset('storage/' . $p->image) }}" alt="">
+                                        <div class="product-action">
+                                            <a class="btn btn-outline-dark btn-square" href=""><i
+                                                    class="fa fa-shopping-cart"></i></a>
+                                            <a class="btn btn-outline-dark btn-square" href=""><i
+                                                    class="fa-solid fa-circle-info"></i></a>
 
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="text-center py-4">
-                                    <a class="h6 text-decoration-none text-truncate" href="">{{ $p->name }}</a>
-                                    <div class="d-flex align-items-center justify-content-center mt-2">
-                                        <h5>{{ $p->price }} kyats</h5>
-                                        <h6 class="text-muted ml-2"><del>25000</del></h6>
-                                    </div>
-                                    <div class="d-flex align-items-center justify-content-center mb-1">
-                                        <small class="fa fa-star text-primary mr-1"></small>
-                                        <small class="fa fa-star text-primary mr-1"></small>
-                                        <small class="fa fa-star text-primary mr-1"></small>
-                                        <small class="fa fa-star text-primary mr-1"></small>
-                                        <small class="fa fa-star text-primary mr-1"></small>
+                                    <div class="text-center py-4">
+                                        <a class="h6 text-decoration-none text-truncate"
+                                            href="">{{ $p->name }}</a>
+                                        <div class="d-flex align-items-center justify-content-center mt-2">
+                                            <h5>{{ $p->price }} kyats</h5>
+                                            <h6 class="text-muted ml-2"><del>25000</del></h6>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-center mb-1">
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
 
                 </div>
             </div>
@@ -149,7 +152,44 @@
                         },
                         dataType: 'json',
                         success: function(response) {
-                            console.log(response);
+                            $list = '';
+
+                            $.each(response, function(index, value) {
+                                $list += `
+                                    <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
+                                <div class="product-item bg-light mb-4">
+                                    <div class="product-img position-relative overflow-hidden">
+                                        <img class="img-fluid w-100" style="height:220px"
+                                            src="{{ asset('storage/${value.image}') }}" alt="">
+                                        <div class="product-action">
+                                            <a class="btn btn-outline-dark btn-square" href=""><i
+                                                    class="fa fa-shopping-cart"></i></a>
+                                            <a class="btn btn-outline-dark btn-square" href=""><i
+                                                    class="fa-solid fa-circle-info"></i></a>
+
+                                        </div>
+                                    </div>
+                                    <div class="text-center py-4">
+                                        <a class="h6 text-decoration-none text-truncate"
+                                            href="">${value.name}</a>
+                                        <div class="d-flex align-items-center justify-content-center mt-2">
+                                            <h5>${value.price} kyats</h5>
+                                            <h6 class="text-muted ml-2"><del>25000</del></h6>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-center mb-1">
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                `;
+                            });
+
+                            $('#dataList').html($list);
                         }
                     });
                 } else if ($eventOption === 'desc') {
@@ -161,7 +201,44 @@
                         },
                         dataType: 'json',
                         success: function(response) {
-                            console.log(response);
+                            $list = '';
+
+                            $.each(response, function(index, value) {
+                                $list += `
+                                    <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
+                                <div class="product-item bg-light mb-4">
+                                    <div class="product-img position-relative overflow-hidden">
+                                        <img class="img-fluid w-100" style="height:220px"
+                                            src="{{ asset('storage/${value.image}') }}" alt="">
+                                        <div class="product-action">
+                                            <a class="btn btn-outline-dark btn-square" href=""><i
+                                                    class="fa fa-shopping-cart"></i></a>
+                                            <a class="btn btn-outline-dark btn-square" href=""><i
+                                                    class="fa-solid fa-circle-info"></i></a>
+
+                                        </div>
+                                    </div>
+                                    <div class="text-center py-4">
+                                        <a class="h6 text-decoration-none text-truncate"
+                                            href="">${value.name}</a>
+                                        <div class="d-flex align-items-center justify-content-center mt-2">
+                                            <h5>${value.price} kyats</h5>
+                                            <h6 class="text-muted ml-2"><del>25000</del></h6>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-center mb-1">
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                            <small class="fa fa-star text-primary mr-1"></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                `;
+                            });
+
+                            $('#dataList').html($list);
                         }
                     });
                 }
